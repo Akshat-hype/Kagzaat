227edaab54f22941dea7aafb9d2faf38f0b2d0e0
sarafkushagra <ksaraf0004@gmail.com>
2025-04-12 00:35:11 +0530

    adding upload option

--- frontend/src/components/MainPage.jsx
+++ frontend/src/components/MainPage.jsx
@@ -4,6 +4,7 @@ import Card from "./Card";
 import GettingStarted from "./GettingStarted";
 import DashBoard from "./DashBoard";
 import More from "./More";
+import FileUploadCard from "./Fileupload";
 
 import React from "react";
 
@@ -11,6 +12,7 @@ export default function MainPage({ userName }) {
   return (
     <div>
       <Navbar user={ userName }/>
+      <FileUploadCard />
       <Card />
       <DashBoard />
       <GettingStarted />
